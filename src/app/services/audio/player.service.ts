b76c9b4e7c875f777c5d49b390110908710f9ed9
SALL <sallibrahima1992@gmail.com>
2021-09-14 19:30:11 +0200

    test

--- src/app/services/audio/player.service.ts
+++ src/app/services/audio/player.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
+import * as moment from 'moment';
+import { BehaviorSubject, Subject, Observable } from 'rxjs';
 // import * as moment from 'moment';
 // import { BehaviorSubject, Observable, Subject } from 'rxjs';
-// import { StreamState } from 'src/app/interfaces/stream-state';
+import { StreamState } from 'src/app/interfaces/stream-state';
 // import { takeUntil } from 'rxjs/operators';
 import { AudioCloudService } from './audio-cloud.service';
 
@@ -13,7 +15,7 @@ export class PlayerService {
   constructor(
     private cloudService: AudioCloudService
     ) { 
-    // this.currentSong.load();
+    this.currentSong.load();
     // this.playStream(this.currentSong).subscribe();
   }
   
@@ -24,252 +26,253 @@ export class PlayerService {
     return this.songs;
   }
 
-//   musicInfoFrame:string = 'closed';
+  musicInfoFrame:string = 'closed';
 
-//   playstop:string = 'stop';
+  playstop:string = 'stop';
 
-//   indexOfSongToPlay = 0;
+  indexOfSongToPlay = 0;
 
-//   isPlaying:boolean = false;
+  isPlaying:boolean = false;
 
-//   currentSong = new Audio(this.songs[this.indexOfSongToPlay].songUrl);
+  currentSong = new Audio(this.songs[this.indexOfSongToPlay].songUrl);
 
-//   timer = this.currentSong.currentTime;
+  timer = this.currentSong.currentTime;
 
-//   private observable: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  private observable: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-//   private songindex  : BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  private songindex  : BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
-//   private songDuration : BehaviorSubject<string> = new BehaviorSubject<string>(this.formatTime(this.currentSong.duration));
+  private songDuration : BehaviorSubject<string> = new BehaviorSubject<string>(this.formatTime(this.currentSong.duration));
 
-//   private songVolume: BehaviorSubject<number> = new BehaviorSubject<number>(0.5);
+  private songVolume: BehaviorSubject<number> = new BehaviorSubject<number>(0.5);
 
-//   getSongVolume = () =>{
-//     return this.songVolume;
-//   }
+  getSongVolume = () =>{
+    return this.songVolume;
+  }
 
-//   getSongDuration = () =>{
-//     return this.songDuration;
-//   }
+  getSongDuration = () =>{
+    return this.songDuration;
+  }
 
-//   getobservable = () =>{
-//     return this.observable;
-//   }
+  getobservable = () =>{
+    return this.observable;
+  }
   
-//   getSongIndex = () =>{
-//     return this.songindex;
-//   }
+  getSongIndex = () =>{
+    return this.songindex;
+  }
 
-//   private stop$ = new Subject();
+  private stop$ = new Subject();
 
-//   private state: StreamState = {
-//     playing: false,
-//     readableCurrentTime: '',
-//     readableDuration: '',
-//     duration: undefined,
-//     currentTime: undefined,
-//     canplay: false,
-//     error: false,
-//   };
+  private state: StreamState = {
+    playing: false,
+    readableCurrentTime: '',
+    readableDuration: '',
+    duration: undefined,
+    currentTime: undefined,
+    canplay: false,
+    error: false,
+  };
 
-//   private stateChange: BehaviorSubject<StreamState> = new BehaviorSubject(
-//     this.state
-//   );
+  private stateChange: BehaviorSubject<StreamState> = new BehaviorSubject(
+    this.state
+  );
 
-//   private resetState() {
-//     this.state = {
-//       playing: false,
-//       readableCurrentTime: '',
-//       readableDuration: '',
-//       duration: undefined,
-//       currentTime: undefined,
-//       canplay: false,
-//       error: false
-//     };
-//   }
+  private resetState() {
+    this.state = {
+      playing: false,
+      readableCurrentTime: '',
+      readableDuration: '',
+      duration: undefined,
+      currentTime: undefined,
+      canplay: false,
+      error: false
+    };
+  }
 
-//   getState(): Observable<StreamState> {
-//     return this.stateChange.asObservable();
-//   }
+  getState(): Observable<StreamState> {
+    return this.stateChange.asObservable();
+  }
 
-//   private updateStateEvents(event: Event): void {
-//     switch (event.type) {
-//       case "canplay":
-//         this.state.duration = this.currentSong.duration;
-//         this.state.readableDuration = this.formatTime(this.state.duration);
-//         this.state.canplay = true;
-//         break;        
-//       case "playing":        
-//         this.state.playing = true;
-//         break;
-//       case "pause":
-//         this.state.playing = false;
-//         break;
-//       case "timeupdate":
-//         this.state.currentTime = this.currentSong.currentTime;
-//         this.state.readableCurrentTime = this.formatTime(
-//         this.state.currentTime
-//         );
-//         break;
-//       case "error":
-//         this.resetState();
-//         this.state.error = true;
-//         break;
-//     }
-//     this.stateChange.next(this.state);
-//   }
-//   stop() {
-//     this.stop$.next();
-//   }
+  private updateStateEvents(event: Event): void {
+    switch (event.type) {
+      case "canplay":
+        this.state.duration = this.currentSong.duration;
+        this.state.readableDuration = this.formatTime(this.state.duration);
+        this.state.canplay = true;
+        break;        
+      case "playing":        
+        this.state.playing = true;
+        break;
+      case "pause":
+        this.state.playing = false;
+        break;
+      case "timeupdate":
+        this.state.currentTime = this.currentSong.currentTime;
+        this.state.readableCurrentTime = this.formatTime(
+        this.state.currentTime
+        );
+        break;
+      case "error":
+        this.resetState();
+        this.state.error = true;
+        break;
+    }
+    this.stateChange.next(this.state);
+  }
+  stop() {
+    this.stop$.next();
+  }
 
-//   seekTo(seconds:number) {
-//     console.log(seconds);
-//     this.currentSong.currentTime = seconds;
-//   }
-//   changevolume(newVolume:number) {
-//     this.currentSong.volume=newVolume;
-//     this.songVolume.next(newVolume);
-//   }
+  seekTo(seconds:number) {
+    console.log(seconds);
+    this.currentSong.currentTime = seconds;
+  }
+  changevolume(newVolume:number) {
+    this.currentSong.volume=newVolume;
+    this.songVolume.next(newVolume);
+  }
 
-//   formatTime(time: number, format: string = 'mm:ss') {
-//     const momentTime = time * 1000;
-//     return moment.utc(momentTime).format(format);
-//   }
-//   getCurrentSong = ():HTMLAudioElement=>{
-//     return this.currentSong;
-//   }
+  formatTime(time: number, format: string = 'mm:ss') {
+    const momentTime = time * 1000;
+    return moment.utc(momentTime).format(format);
+  }
+  getCurrentSong = ():HTMLAudioElement=>{
+    return this.currentSong;
+  }
 
-//   audioEvents = [
-//     "ended",
-//     "error",
-//     "play",
-//     "playing",
-//     "pause",
-//     "timeupdate",
-//     "canplay",
-//     // "loadedmetadata",
-//     // "loadstart",
-//   ];
+  audioEvents = [
+    "ended",
+    "error",
+    "play",
+    "playing",
+    "pause",
+    "timeupdate",
+    "canplay",
+    // "loadedmetadata",
+    // "loadstart",
+  ];
 
-// getStateReadableDuration = ()=>{
-//   return this.state.readableDuration;
-// }
-// private addEvents(audioElement:any, events:string[], handler:Function) {
-//   events.forEach((event:string) => {
-//     audioElement.addEventListener(event, handler);
-//   });
-// }
+getStateReadableDuration = ()=>{
+  return this.state.readableDuration;
+}
+private addEvents(audioElement:any, events:string[], handler:Function) {
+  events.forEach((event:string) => {
+    audioElement.addEventListener(event, handler);
+  });
+}
 
-// private removeEvents(audioElement:any, events:string[], handler:Function) {
-//   events.forEach((event:string) => {
-//     audioElement.removeEventListener(event, handler);
-//   });
-// }
+private removeEvents(audioElement:any, events:string[], handler:Function) {
+  events.forEach((event:string) => {
+    audioElement.removeEventListener(event, handler);
+  });
+}
 
-// private AudioObservable(audioElement:HTMLAudioElement){  
-//   return new Observable((observer)=>{    
-//     // audioElement.load();
-//     // audioElement.play();
-//     const handler=(event:Event)=>{
-//       this.updateStateEvents(event);
-//       observer.next(event);
-//     }
-//     this.addEvents(audioElement, this.audioEvents, handler);
-//     return () => {
-//       // Stop Playing
-//       audioElement.pause();
-//       audioElement.currentTime = 0;
-//       console.log('observable initialisé');
-//       // remove event listeners
-//       this.removeEvents(audioElement, this.audioEvents, handler);
-//     };
-//   });
-// }
+private AudioObservable(audioElement:HTMLAudioElement){  
+  return new Observable((observer)=>{    
+    // audioElement.load();
+    // audioElement.play();
+    const handler=(event:Event)=>{
+      this.updateStateEvents(event);
+      observer.next(event);
+    }
+    this.addEvents(audioElement, this.audioEvents, handler);
+    return () => {
+      // Stop Playing
+      audioElement.pause();
+      audioElement.currentTime = 0;
+      console.log('observable initialisé');
+      // remove event listeners
+      this.removeEvents(audioElement, this.audioEvents, handler);
+    };
+  });
+}
 
-// playStream(audioElement:HTMLAudioElement) {
-//   return this.AudioObservable(audioElement).pipe(takeUntil(this.stop$));
-// }
+playStream(audioElement:HTMLAudioElement) {
+  // return this.AudioObservable(audioElement).pipe(takeUntil(this.stop$));
+  return this.AudioObservable(audioElement);
+}
 
-// playCurrentSong = () => {
-//   // this.currentSong.play();
-//   this.musicInfoFrame='opened';
-//   this.isPlaying=true;
-//   this.playstop='play';  
-//   console.log(this.playstop);
-//   this.observable.next(this.isPlaying);
-//   // this.playStream(this.currentSong).subscribe();
-//   this.currentSong.play();
-// }
+playCurrentSong = () => {
+  // this.currentSong.play();
+  this.musicInfoFrame='opened';
+  this.isPlaying=true;
+  this.playstop='play';  
+  console.log(this.playstop);
+  this.observable.next(this.isPlaying);
+  // this.playStream(this.currentSong).subscribe();
+  this.currentSong.play();
+}
 
 
-// pauseCurrentSong = () => {
-//   this.playstop="pause";
-//   this.currentSong.pause();
-//   this.isPlaying=false;
-//   console.log(this.playstop);
-//   this.observable.next(this.isPlaying); 
-// }
+pauseCurrentSong = () => {
+  this.playstop="pause";
+  this.currentSong.pause();
+  this.isPlaying=false;
+  console.log(this.playstop);
+  this.observable.next(this.isPlaying); 
+}
 
-// nextSong = () => {
-//   this.currentSong.pause();
-//   if(this.indexOfSongToPlay === ( this.songs.length - 1 )){
-//     this.indexOfSongToPlay = 0;
-//   }else{
-//     this.indexOfSongToPlay+=1;
-//   }
-//   this.songindex.next(this.indexOfSongToPlay);
-//   this.currentSong = new Audio(this.songs[this.indexOfSongToPlay].songUrl);
-//   this.currentSong.load();
-//   if(this.musicInfoFrame==='opened'){
-//     this.playstop='zero';
-//   }
-//   this.isPlaying=false;
+nextSong = () => {
+  this.currentSong.pause();
+  if(this.indexOfSongToPlay === ( this.songs.length - 1 )){
+    this.indexOfSongToPlay = 0;
+  }else{
+    this.indexOfSongToPlay+=1;
+  }
+  this.songindex.next(this.indexOfSongToPlay);
+  this.currentSong = new Audio(this.songs[this.indexOfSongToPlay].songUrl);
+  this.currentSong.load();
+  if(this.musicInfoFrame==='opened'){
+    this.playstop='zero';
+  }
+  this.isPlaying=false;
   
-//   this.playStream(this.currentSong).subscribe();
-//   this.stateChange.next(this.state);
-//   this.songDuration.next(this.state.readableDuration);
+  this.playStream(this.currentSong).subscribe();
+  this.stateChange.next(this.state);
+  this.songDuration.next(this.state.readableDuration);
 
-// }
+}
 
-// previousSong = () => {
-//   this.currentSong.pause();
-//   if(this.musicInfoFrame==='opened'){
-//     this.playstop='zero';
-//   }
-//   if(this.indexOfSongToPlay === 0){
-//     this.indexOfSongToPlay = this.songs.length - 1;
-//   }else{
-//     this.indexOfSongToPlay-=1;
-//   }
-//   // this.currentSong.load();
-//   if(this.isPlaying){
-//     this.currentSong.play();
-//   }
+previousSong = () => {
+  this.currentSong.pause();
+  if(this.musicInfoFrame==='opened'){
+    this.playstop='zero';
+  }
+  if(this.indexOfSongToPlay === 0){
+    this.indexOfSongToPlay = this.songs.length - 1;
+  }else{
+    this.indexOfSongToPlay-=1;
+  }
+  // this.currentSong.load();
+  if(this.isPlaying){
+    this.currentSong.play();
+  }
   
-//   this.currentSong = new Audio(this.songs[this.indexOfSongToPlay].songUrl);
-//   this.songDuration.next(this.formatTime(this.currentSong.duration));
-//   this.currentSong.load();
-//   this.playStream(this.currentSong).subscribe();
-//   this.observable.next(this.isPlaying);
-//   this.songindex.next(this.indexOfSongToPlay);
-//   this.stateChange.next(this.state);
-// }
+  this.currentSong = new Audio(this.songs[this.indexOfSongToPlay].songUrl);
+  this.songDuration.next(this.formatTime(this.currentSong.duration));
+  this.currentSong.load();
+  this.playStream(this.currentSong).subscribe();
+  this.observable.next(this.isPlaying);
+  this.songindex.next(this.indexOfSongToPlay);
+  this.stateChange.next(this.state);
+}
 
-// stopSong = () => {
-//   this.currentSong.load();
-//   this.isPlaying=false;
-//   this.playstop='stop';
-//   console.log(this.playstop);
-//   this.observable.next(this.isPlaying);
-//   this.stop$.next();
-//   this.playStream(this.currentSong).subscribe();
-//   this.musicInfoFrame="closed";
-// }
+stopSong = () => {
+  this.currentSong.load();
+  this.isPlaying=false;
+  this.playstop='stop';
+  console.log(this.playstop);
+  this.observable.next(this.isPlaying);
+  this.stop$.next();
+  this.playStream(this.currentSong).subscribe();
+  this.musicInfoFrame="closed";
+}
 
-// replaySong = () => {    
-//   this.currentSong.load();
-//   this.currentSong.play();
-//   this.isPlaying=true;
-// }
+replaySong = () => {    
+  this.currentSong.load();
+  this.currentSong.play();
+  this.isPlaying=true;
+}
   
 }
