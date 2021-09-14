3e46af81a3584b1469ed6c5270ce8da2b73bb3d1
SALL <sallibrahima1992@gmail.com>
2021-09-14 16:43:05 +0200

    test

--- src/app/components/audio-player/audio-player.component.ts
+++ src/app/components/audio-player/audio-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerService } from 'src/app/services/audio/player.service';
 
 @Component({
   selector: 'app-audio-player',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AudioPlayerComponent implements OnInit {
 
-  constructor() { }
+  constructor(playerservice:PlayerService) {
+    console.log(playerservice.getSongIndex());
+   }
 
   ngOnInit(): void {
   }
