d50752d84f0c029af6918bc247a9b5fbe4088d3b
SALL <sallibrahima1992@gmail.com>
2021-09-15 01:44:11 +0200

    test

--- src/app/components/audio-player/audio-player.component.ts
+++ src/app/components/audio-player/audio-player.component.ts
@@ -9,43 +9,22 @@ import { PlayerService } from 'src/app/services/audio/player.service';
 })
 export class AudioPlayerComponent implements OnInit {
   constructor(public playerService:PlayerService){
-    this.playerService.getSongIndex().subscribe((value)=>{
-      console.log(value);
-      this.indexOfSongToPlay=value;
-    });
-    this.playerService.getState().subscribe(state => {
-      this.state = state;
-    });
-    this.playerService.getSongDuration().subscribe(readableDuration =>{
-      this.dura=""+readableDuration;
-      console.log(this.dura);
-    });
-    this.playerService.getSongVolume().subscribe((volume:number)=>{this.volumeState=volume;console.log(this.volumeState);});
+    // this.playerService.getSongIndex().subscribe((value)=>{
+    //   console.log(value);
+    //   this.indexOfSongToPlay=value;
+    // });
+    // this.playerService.getState().subscribe(state => {
+    //   this.state = state;
+    // });
+    // this.playerService.getSongDuration().subscribe(readableDuration =>{
+    //   this.dura=""+readableDuration;
+    //   console.log(this.dura);
+    // });
+    // this.playerService.getSongVolume().subscribe((volume:number)=>{this.volumeState=volume;console.log(this.volumeState);});
   }
   ngOnInit(): void {
 
   }
-  state:StreamState | undefined;
-  dura:string="";
-  
-  volumeState:number=0.5;
 
-  getStateReadableDuration = this.playerService.getStateReadableDuration;
-  
-  playin:boolean=false;
-  
-  songs=this.playerService.getSongs();
-  
-  playstop = () => {
-    return this.playerService.playstop;
-  }
-  
-  MusicInfoFrame = () =>{
-    return this.playerService.musicInfoFrame;
-  }
-  
-  showPlayer=true;
-  
-  indexOfSongToPlay=this.playerService.indexOfSongToPlay;
-  
+
 }
