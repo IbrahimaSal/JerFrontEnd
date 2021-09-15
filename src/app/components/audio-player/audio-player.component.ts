6c94a9385d51d6be257ba64d01436a552686064e
SALL <sallibrahima1992@gmail.com>
2021-09-15 02:19:26 +0200

    test

--- src/app/components/audio-player/audio-player.component.ts
+++ src/app/components/audio-player/audio-player.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { StreamState } from 'src/app/interfaces/stream-state';
 import { PlayerService } from 'src/app/services/audio/player.service';
 
 @Component({
@@ -9,18 +8,7 @@ import { PlayerService } from 'src/app/services/audio/player.service';
 })
 export class AudioPlayerComponent implements OnInit {
   constructor(public playerService:PlayerService){
-    // this.playerService.getSongIndex().subscribe((value)=>{
-    //   console.log(value);
-    //   this.indexOfSongToPlay=value;
-    // });
-    // this.playerService.getState().subscribe(state => {
-    //   this.state = state;
-    // });
-    // this.playerService.getSongDuration().subscribe(readableDuration =>{
-    //   this.dura=""+readableDuration;
-    //   console.log(this.dura);
-    // });
-    // this.playerService.getSongVolume().subscribe((volume:number)=>{this.volumeState=volume;console.log(this.volumeState);});
+   
   }
   ngOnInit(): void {
 
